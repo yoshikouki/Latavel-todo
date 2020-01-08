85d2f7a908717997316807f455c64212a5cba6eb
吉本康貴 <yoshikouki@K2.local>
2020-01-08 16:51:17 +0900

    varchar 191を実行環境へ移動

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (\App::environment('production')) {
         \URL::forceScheme('https');
-        Schema::defaultStringLength(191);
         }
+        Schema::defaultStringLength(191);
     }
 }
